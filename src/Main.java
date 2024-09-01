2cb88db54aeb935a83bd7152e7c98008c0f090be
S1thK3nny <kennethildebrandt@gmail.com>
2024-09-01 22:30:52 +0200

    Added a measurement of time to see how long it took to solve the sudoku, also added a print incase that it is impossible to solve.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Sudoku sud = new Sudoku();
         System.out.println(sud);
-        sud.solveSudoku();
-        System.out.println(sud);
+        long startTime = System.currentTimeMillis();
+        if(sud.solveSudoku()) {
+            long estimatedTime = System.currentTimeMillis() - startTime;
+            System.out.println(sud);
+            System.out.println("This sudoku was solved in " + estimatedTime + " seconds!");
+        }
+        else {
+            System.out.println("There is no solution for this sudoku. (Sad!)");
+        }
     }
 }
